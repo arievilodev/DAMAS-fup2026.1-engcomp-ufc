226b10e561e8e02649649102063500508da325df
ArthurDev-A <arthurdearaujocustodio0@gmail.com>
2026-07-01 23:51:07 -0300

    Ajeitando o nome do Arthur nos cometários

--- Damas/jogo.c
+++ Damas/jogo.c
@@ -2,7 +2,7 @@
     Created by Arthur de Araujo Custodio on 23/06/2026.
     Copyright © 2026 
         Ana Rebeca Mendes de Souza (602242);
-        Arthur Araujo Custodio (604827);
+        Arthur de Araujo Custodio (604827);
         Rafael Calisto Oliveira da Silva (606478).
     All rights reserved.
 */
